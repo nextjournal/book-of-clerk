d74362039690a4505f15a61112cab7da0615e2b8
Martin Kavalar <martin@nextjournal.com>
2022-09-22 09:11:24 -0500

    Also copy book

--- dev/builder.clj
+++ dev/builder.clj
@@ -5,6 +5,7 @@
 (defn copy-files! []
   (let [clerk-root (-> (io/resource "nextjournal/clerk.clj") fs/parent fs/parent fs/parent)]
     (fs/copy (fs/file clerk-root "book.clj") (fs/file "index.clj") #{:replace-existing})
+    (fs/copy (fs/file clerk-root "book.clj") (fs/file "book.clj") #{:replace-existing})
     (fs/copy (fs/file clerk-root "chinook.db") (fs/file "chinook.db") #{:replace-existing})))
 
 (defn copy-files+get-paths []
