1601ac9afd5fea384c097529de5b7f994f68c53c
Martin Kavalar <martin@nextjournal.com>
2022-09-22 07:30:57 -0500

    Initial release

---
+++ dev/builder.clj
@@ -0,0 +1,12 @@
+(ns builder
+  (:require [babashka.fs :as fs]
+            [clojure.java.io :as io]))
+
+(defn copy-files! []
+  (let [clerk-root (-> (io/resource "nextjournal/clerk.clj") fs/parent fs/parent fs/parent)]
+    (fs/copy (fs/file clerk-root "book.clj") (fs/file "index.clj") #{:replace-existing})
+    (fs/copy (fs/file clerk-root "chinook.db") (fs/file "chinook.db") #{:replace-existing})))
+
+(defn copy-files+get-paths []
+  (copy-files!)
+  ["index.clj"])
